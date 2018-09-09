2ceb23aac418e5a507bbdf8a96305610d70a31ab
Ehud Adler <easports96@gmail.com>
2018-09-09 18:37:08 -0400

    Removed the branches. WIP

--- Classes/Views/GitHawkRefreshControl.swift
+++ Classes/Views/GitHawkRefreshControl.swift
@@ -10,21 +10,40 @@ import UIKit
 
 class GitHawkRefreshControl: UIRefreshControl {
   
-  public var progress: CGFloat = 0.0
-
-  private let BIRD_INSET:CGFloat = 20
-  private let REFRESH_INSET:CGFloat = 5
-  private let DEGRESS_TO_ROTATE:CGFloat = 180
+  override var frame: CGRect {
+    get { return super.frame }
+    set {
+      var newFrame = newValue
+      if let superScrollView = superview as? UIScrollView {
+        update(y: superScrollView.contentOffset.y)
+        newFrame.origin.x = superScrollView.frame.minX - superScrollView.contentInset.left
+      }
+      super.frame = newFrame
+    }
+  }
   
-  // The amount behind the pull to refresh frame where the animation will continue until
-  private let RUBBER_BAND_AMOUNT:CGFloat = 180
-
+  public var progress: CGFloat = 0.0 {
+    didSet {
+      scale(percent: progress)
+      setPlacement(percent: progress)
+    }
+  }
+  private var startingHeight: CGFloat = 0
   
+  private struct Insets {
+    static let bird:CGFloat = 20
+    static let refresh:CGFloat = 5
+  }
+  
+  private var isPulsing = false
+
   private let birdImageView = UIImageView(image: #imageLiteral(resourceName: "splash"))
-  private let branchesImageView = UIImageView(image: #imageLiteral(resourceName: "splash_branches"))
+  private let contentView = UIView()
   
   // The tableview could start at a different Y offset.
   private var tableViewInitialYOffset: CGFloat?
+  
+  required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
   
   required override init() {
     super.init()
@@ -32,90 +51,95 @@ class GitHawkRefreshControl: UIRefreshControl {
     setUpView()
   }
   
-  override func didMoveToSuperview() {
-      super.didMoveToSuperview()
-      guard let sup = self.superview as? UIScrollView else { return }
-      self.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height * 2) // Double height for bigger image
-      tableViewInitialYOffset = sup.contentOffset.y
-      sup.addObserver(self,
-                      forKeyPath: #keyPath(UIScrollView.contentOffset), options: [.old, .new], context: nil)
-
-  }
-  
   override func beginRefreshing() {
     super.beginRefreshing()
-    _addBirdAnimation()
+    addBirdAnimation()
   }
   
   override func endRefreshing() {
     super.endRefreshing()
-    birdImageView.layer.removeAllAnimations()
+    progress = 0
   }
   
   private func setUpView() {
+    startingHeight = self.frame.height
+    let imageHeight = startingHeight - Insets.refresh
     
-    let imageHeight = self.frame.height - REFRESH_INSET // 10 padding
-
+    // ContentView
+    self.addSubview(contentView)
+    contentView.translatesAutoresizingMaskIntoConstraints = false
+    contentView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    contentView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    contentView.widthAnchor.constraint(equalToConstant: imageHeight).isActive = true
+    contentView.heightAnchor.constraint(equalToConstant: imageHeight).isActive = true
+    
     // Bird
-    self.addSubview(birdImageView)
-    
+    contentView.addSubview(birdImageView)
     birdImageView.translatesAutoresizingMaskIntoConstraints = false
-    birdImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    birdImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    birdImageView.widthAnchor.constraint(equalToConstant: imageHeight - BIRD_INSET).isActive = true
-    birdImageView.heightAnchor.constraint(equalToConstant: imageHeight - BIRD_INSET).isActive = true
-    
-    // Branches
-    self.addSubview(branchesImageView)
-    
-    branchesImageView.translatesAutoresizingMaskIntoConstraints = false
-    branchesImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    branchesImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    branchesImageView.widthAnchor.constraint(equalToConstant: imageHeight).isActive = true
-    branchesImageView.heightAnchor.constraint(equalToConstant: imageHeight).isActive = true
-    
+    birdImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+    birdImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+    birdImageView.widthAnchor.constraint(equalToConstant: imageHeight - Insets.bird).isActive = true
+    birdImageView.heightAnchor.constraint(equalToConstant: imageHeight - Insets.bird).isActive = true
   }
   
-  override func observeValue(forKeyPath keyPath: String?,
-                             of object: Any?,
-                             change: [NSKeyValueChangeKey : Any]?,
-                             context: UnsafeMutableRawPointer?) {
+  private func update(y yOffset: CGFloat) {
     
-    if keyPath == #keyPath(UIScrollView.contentOffset) {
-      guard let scrollView = object as? UIScrollView,
-        let initialYOffset = tableViewInitialYOffset else { return }
-      
-      let adjOffset = initialYOffset - scrollView.contentOffset.y
-      if(adjOffset > 0) // > 0 because both initialYOffset & scrollView.contentOffset.y are 0 when pulling
-      {
-        self.updateProgress(adjOffset)
-      }
+    let adjOffset = -yOffset - startingHeight
+    // Check if we have passed the point where the refresh control kicks in
+    if !isPulsing && adjOffset > startingHeight {
+      addBirdAnimation()
+    }
+    
+    if adjOffset > 0 {
+      self.updateProgress(adjOffset)
     }
   }
   
-  private func updateProgress(_ yOffset: CGFloat)
-  {
-    progress = (yOffset * 100 / (self.frame.height + RUBBER_BAND_AMOUNT)) / 100 // Convert to 0 - 1 scale
-    if(progress >= 0 && progress <= 1)
-    {
-      branchesImageView.transform = CGAffineTransform(rotationAngle: (DEGRESS_TO_ROTATE * progress) * (CGFloat.pi / 180))
-    }
+  private func updateProgress(_ yOffset: CGFloat) {
+    // Convert to 0 - 1 scale
+    progress = (yOffset * 100 / startingHeight) / 100
   }
   
-  required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+  private func setPlacement(percent: CGFloat) {
+    let adjPercent = percent < 1
+      ? -startingHeight + (percent * startingHeight)
+      : 1
+    contentView.transform = CGAffineTransform(
+      translationX: 1,
+      y: adjPercent
+    )
+  }
   
-  private func _addBirdAnimation()
-  {
+  private func scale(percent: CGFloat) {
+    let adjPercent = percent < 1
+      ? percent
+      : 1
+    self.transform = CGAffineTransform(
+      scaleX: adjPercent,
+      y: adjPercent
+    )
+  }
+  
+  private func addBirdAnimation() {
+    
+    isPulsing = true
+
     let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
     
     pulseAnimation.autoreverses = true
     pulseAnimation.repeatCount = .infinity
     pulseAnimation.fromValue = 1.0
-    pulseAnimation.toValue = 0.9
+    pulseAnimation.toValue = 0.7
     pulseAnimation.duration = 1
     pulseAnimation.fillMode = kCAFillModeForwards
-    pulseAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    pulseAnimation.timingFunction = CAMediaTimingFunction(
+      name: kCAMediaTimingFunctionEaseInEaseOut
+    )
     
-    birdImageView.layer.add(pulseAnimation, forKey: "scaling")
+    birdImageView.layer.add(
+      pulseAnimation,
+      forKey: "scaling"
+    )
   }
 }
+
